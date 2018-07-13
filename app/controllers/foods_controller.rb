346323c7359352412e28cbfd119082f33166c358
sabin262 <sabin262@gmail.com>
2018-07-13 16:58:55 +0545

    Add Delete and show

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,26 +1,52 @@
 class FoodsController < ApplicationController
+	before_action :set_food, only: [:edit, :update, :show, :destroy]
 	def index
 		@food = Food.all
+	end
+	
+	def show
 	end
 	
 	def new
 		@food = Food.new
 	end
 
-
 	def create
 		@food = Food.new(food_params)
 		if @food.save
 			flash[:success] = "Food Saved!"
-			redirect_to root_path
+			redirect_to foods_path
 		else
 			render 'new'
 		end
+	end
 
+	def edit
+	end
+
+	def update
+		if @food.update(food_params)
+			flash[:success] = "Update Successful!"
+			redirect_to foods_path(@food)
+		else
+			render 'edit'
+		end
+	end
+
+
+	def destroy
+		@food.destroy
+		redirect_to foods_path
 	end
 
 	private
+		def set_food
+			@food = Food.find(params[:id])
+		end
+
 		def food_params
 			params.require(:food).permit(:name, :price, :category)
 		end
+
+		
 end
